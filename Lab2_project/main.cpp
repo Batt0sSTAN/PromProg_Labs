df9978e3cf20b95ee936ae40124691c7fcddc211
Batt0sSTAN <battos.work@gmail.com>
2026-06-01 16:34:31 +0300

    Updated .gitignore

--- Lab2_project/main.cpp
+++ Lab2_project/main.cpp
@@ -9,9 +9,7 @@ void showMenu() {
 
 int main() {
     std::cout << std::endl; // Отступ для читаемости
-
-    std::cout << "--- ТУТ БЫЛ РАЗРАБОТЧИК 2 ---" << std::endl;
-
+    
     std::cout << "--- РАБОТАЕТ РАЗРАБОТЧИК 1 ---" << std::endl;
 
     showMenu(); 
